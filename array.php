958cf5fe065faaec814bbdfbfa51ecaad5c931b2
dsilvaEVG <dsilvavega.guadalupe@alumnado.fundacionloyola.net>
2024-09-27 08:42:35 +0200

    Update array.php

--- array.php
+++ array.php
@@ -1,3 +1,4 @@
+<!-- David Silva Vega -->
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -7,14 +8,17 @@
         <title>Array con imagenes</title>
     </head>
     <body>
-        <table>
+        <!-- Metemos el array en una tabla, con una sola fila (tr) -->
+        <table> 
             <tr>
                 <?php
                     $imagenes = array(
                     "flor.jpg" => "Flor",
                     "coche.jpg" => "Coche",
                     "casa.jpg" => "Casa");
+                    // El índice será el nombre del archivo, el valor será el nombre de la imagen
 
+                    // Vamos a pasar con el foreach el listado de elementos de la array asociativa
                     foreach($imagenes as $ruta => $nombre){
                         echo '<td><figure><img src="img/'.$ruta.'">
                         <figcaption>'.$nombre.'</figcaption></figure></td>';
