19444a838c8c96213d43f26eb71595110366676e
dsilvaEVG <dsilvavega.guadalupe@alumnado.fundacionloyola.net>
2024-09-27 08:36:03 +0200

    Update array.php

--- array.php
+++ array.php
@@ -1,29 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel=stylesheet href=estilo.css />
-    <title>Array con imagenes</title>
-</head>
-<body>
-    <table>
-        <tr>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel=stylesheet href=estilo.css />
+        <title>Array con imagenes</title>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <?php
+                    $imagenes = array(
+                    "flor.jpg" => "Flor",
+                    "coche.jpg" => "Coche",
+                    "casa.jpg" => "Casa");
 
-        <?php
-            $imagenes = array(
-            "flor.jpg" => "Flor",
-            "coche.jpg" => "Coche",
-            "casa.jpg" => "Casa");
-
-                
-            foreach($imagenes as $ruta => $nombre){
-                echo '<td><figure><img src="img/'.$ruta.'">
-                <figcaption>'.$nombre.'</figcaption></figure></td>';
-            }
-        ?>
-</tr>
-</table>
-</body>
+                    foreach($imagenes as $ruta => $nombre){
+                        echo '<td><figure><img src="img/'.$ruta.'">
+                        <figcaption>'.$nombre.'</figcaption></figure></td>';
+                    }
+                ?>
+            </tr>
+        </table>
+    </body>
 </html>
-<body>
